d6664ef21624a81c3dd839b819982f6f1e5c2d2f
lokeslokesh12 <lokesh272203@gmail.com>
2022-09-30 22:05:05 +0530

    last commit

--- dom.js
+++ dom.js
@@ -7,146 +7,112 @@ function add(){
 }
 function equ(){
     let firnum = document.getElementById("first").value;
-    // let secnum = document.getElementById("second").value;
+   
     let output = eval(`${firnum}`)
-    // document.getElementById("msg").innerText = `${firnum} = ${output}`
+   
     document.getElementById("first").value = `${output}`
 
     // console.log(`result ${output}`)
 }
 function num1(){
     let firnum = document.getElementById("first").value;
-    // let secnum = document.getElementById("second").value;
-    // let output = eval(`${firnum}`)
-    // document.getElementById("msg").innerText = `${firnum}1`
+  
     document.getElementById("first").value = `${firnum}1`
     // console.log(`result ${output}`)
 }
 function num2(){
     let firnum = document.getElementById("first").value;
-    // let secnum = document.getElementById("second").value;
-    // let output = eval(`${firnum}`)
-    // document.getElementById("msg").innerText = `${firnum}1`
+   
     document.getElementById("first").value = `${firnum}2`
     // console.log(`result ${output}`)
 }
 function num3(){
     let firnum = document.getElementById("first").value;
-    // let secnum = document.getElementById("second").value;
-    // let output = eval(`${firnum}`)
-    // document.getElementById("msg").innerText = `${firnum}1`
+   
     document.getElementById("first").value = `${firnum}3`
     // console.log(`result ${output}`)
 }
 function num4(){
     let firnum = document.getElementById("first").value;
-    // let secnum = document.getElementById("second").value;
-    // let output = eval(`${firnum}`)
-    // document.getElementById("msg").innerText = `${firnum}1`
+  
     document.getElementById("first").value = `${firnum}4`
     // console.log(`result ${output}`)
 }
 function num5(){
     let firnum = document.getElementById("first").value;
-    // let secnum = document.getElementById("second").value;
-    // let output = eval(`${firnum}`)
-    // document.getElementById("msg").innerText = `${firnum}1`
+   
     document.getElementById("first").value = `${firnum}5`
     // console.log(`result ${output}`)
 }
 function num6(){
     let firnum = document.getElementById("first").value;
-    // let secnum = document.getElementById("second").value;
-    // let output = eval(`${firnum}`)
-    // document.getElementById("msg").innerText = `${firnum}1`
+
     document.getElementById("first").value = `${firnum}6`
     // console.log(`result ${output}`)
 }
 function num7(){
     let firnum = document.getElementById("first").value;
-    // let secnum = document.getElementById("second").value;
-    // let output = eval(`${firnum}`)
-    // document.getElementById("msg").innerText = `${firnum}1`
+   
     document.getElementById("first").value = `${firnum}7`
     // console.log(`result ${output}`)
 }
 function num8(){
     let firnum = document.getElementById("first").value;
-    // let secnum = document.getElementById("second").value;
-    // let output = eval(`${firnum}`)
-    // document.getElementById("msg").innerText = `${firnum}1`
+   
     document.getElementById("first").value = `${firnum}8`
     // console.log(`result ${output}`)
 }
 function num9(){
     let firnum = document.getElementById("first").value;
-    // let secnum = document.getElementById("second").value;
-    // let output = eval(`${firnum}`)
-    // document.getElementById("msg").innerText = `${firnum}1`
+  
     document.getElementById("first").value = `${firnum}9`
     // console.log(`result ${output}`)
 }
 function num0(){
     let firnum = document.getElementById("first").value;
-    // let secnum = document.getElementById("second").value;
-    // let output = eval(`${firnum}`)
-    // document.getElementById("msg").innerText = `${firnum}1`
+   
     document.getElementById("first").value = `${firnum}0`
     // console.log(`result ${output}`)
 }
 function dote(){
     let firnum = document.getElementById("first").value;
-    // let secnum = document.getElementById("second").value;
-    // let output = eval(`${firnum}`)
-    // document.getElementById("msg").innerText = `${firnum}1`
+   
     document.getElementById("first").value = `${firnum}.`
     // console.log(`result ${output}`)
 }
 function plus(){
     let firnum = document.getElementById("first").value;
-    // let secnum = document.getElementById("second").value;
-    // let output = eval(`${firnum}`)
-    // document.getElementById("msg").innerText = `${firnum}1`
+   
     document.getElementById("first").value = `${firnum}+`
     // console.log(`result ${output}`)
 }
 function minus(){
     let firnum = document.getElementById("first").value;
-    // let secnum = document.getElementById("second").value;
-    // let output = eval(`${firnum}`)
-    // document.getElementById("msg").innerText = `${firnum}1`
+   
     document.getElementById("first").value = `${firnum}-`
     // console.log(`result ${output}`)
 }
 function multi(){
     let firnum = document.getElementById("first").value;
-    // let secnum = document.getElementById("second").value;
-    // let output = eval(`${firnum}`)
-    // document.getElementById("msg").innerText = `${firnum}1`
+   
     document.getElementById("first").value = `${firnum}*`
     // console.log(`result ${output}`)
 }
 function div(){
     let firnum = document.getElementById("first").value;
-    // let secnum = document.getElementById("second").value;
-    // let output = eval(`${firnum}`)
-    // document.getElementById("msg").innerText = `${firnum}1`
+   
     document.getElementById("first").value = `${firnum}/`
     // console.log(`result ${output}`)
 }
 function mod(){
     let firnum = document.getElementById("first").value;
-    // let secnum = document.getElementById("second").value;
-    // let output = eval(`${firnum}`)
-    // document.getElementById("msg").innerText = `${firnum}1`
+   
     document.getElementById("first").value = `${firnum}%`
     // console.log(`result ${output}`)
 }
 function clr(){
     let firnum = document.getElementById("first").value;
-    // let secnum = document.getElementById("second").value;
-    // let output = eval(`${firnum}`)
-    // document.getElementById("msg").innerText = `${firnum}1`
+   
     document.getElementById("first").value = ""
     // console.log(`result ${output}`)
 }
